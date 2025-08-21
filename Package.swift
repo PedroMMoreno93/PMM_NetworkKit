ca8ff1e4671df6162ddefe22d82d0bec27d3e35c
Pedro M Moreno <morenogonzalezpedro93@gmail.com>
2025-08-21 20:18:32 +0200

    (feature): package creation

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PMM_NetworkKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PMM_NetworkKit",
+            targets: ["PMM_NetworkKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PMM_NetworkKit"),
+        .testTarget(
+            name: "PMM_NetworkKitTests",
+            dependencies: ["PMM_NetworkKit"]
+        ),
+    ]
+)
